275ad2204e1471eec13f19a392716c3acbbebab5
ak5karthi <akashkumar@fple.in>
2024-01-17 15:17:32 +0530

    gaming Folder

---
+++ 22FUN/src/main/java/Fun22/Fun22/App.java
@@ -0,0 +1,13 @@
+package Fun22.Fun22;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
